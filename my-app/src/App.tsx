afbd826d6b9ebc9e9b6635c115240123f3f0e555
Yujing9 <244242977@qq.com>
2023-02-26 21:37:12 +0000

    react build

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Login.tsx
@@ -0,0 +1,11 @@
+import { Button, Input } from "@chakra-ui/react";
+function Login() {
+  return (
+    <div>
+      <Input placeholder="Username" />
+      <Input placeholder="Password" />
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
+}
+export default Login;
